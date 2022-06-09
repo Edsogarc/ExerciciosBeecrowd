60292ab837401ef871cc567aaab4b39b4d6dd5fc
Edsogarc <nosdesdhm@gmail.com>
2022-06-09 16:30:33 -0300

    Atualização

--- bcrw1018/Program.cs
+++ bcrw1018/Program.cs
@@ -6,7 +6,7 @@ class Program
     static void Main(string[] args)
     {
         
-        int N, quociente, resto, nota;
+        /*int N, quociente, resto, nota;
 
         N = int.Parse(Console.ReadLine());
         Console.WriteLine(N);
@@ -44,7 +44,18 @@ class Program
         resto = resto % nota;
 
         Console.WriteLine(resto + " nota(s) de R$ 1,00");
+        */
+        int n = int.Parse(Console.ReadLine());
+        int[] notes = {100, 50, 20, 10, 5, 2, 1};
 
+        int rest = n;
+        int quotient = 0;
+        foreach (int note in notes)
+        {
+            quotient = rest / note;
+            Console.WriteLine(quotient + " nota (s) de R$ "+note+",00");
+            rest = rest % note;
+        }
     }
 
 }
